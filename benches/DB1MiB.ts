14061074bd4de23b57c32999e430455c1c0247ee
Roger Qiu <roger.qiu@polyhack.io>
2021-09-17 21:21:54 +1000

    Added benchmarks, no crypto or workers

--- benches/DB1MiB.ts
+++ benches/DB1MiB.ts
@@ -1,58 +1,40 @@
-import type { EFSWorkerModule } from '@/workers';
-
 import os from 'os';
+import crypto from 'crypto';
 import path from 'path';
 import fs from 'fs';
 import b from 'benny';
-import { spawn, Worker } from 'threads';
 import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
-import { WorkerManager } from '@matrixai/workers';
-import { DB } from '@/db';
-import * as utils from '@/utils';
+import DB from '@/DB';
 import packageJson from '../package.json';
 
 const logger = new Logger('DB1MiB Bench', LogLevel.WARN, [new StreamHandler()]);
 
 async function main() {
-  const cores = os.cpus().length;
-  logger.warn(`Cores: ${cores}`);
-  const workerManager = new WorkerManager<EFSWorkerModule>({ logger });
-  await workerManager.start({
-    workerFactory: () => spawn(new Worker('../src/workers/efsWorker')),
-    cores,
-  });
   const dataDir = await fs.promises.mkdtemp(
     path.join(os.tmpdir(), 'encryptedfs-benches-'),
   );
-  const dbKey = await utils.generateKey(256);
-  // Db1 doesn't use workers
-  const dbPath1 = `${dataDir}/db1`;
-  const db1 = await DB.createDB({ dbKey, dbPath: dbPath1, logger });
-  await db1.start();
-  // Db2 uses workers
-  const dbPath2 = `${dataDir}/db2`;
-  const db2 = await DB.createDB({ dbKey, dbPath: dbPath2, logger });
-  await db2.start();
-  const data1MiB = utils.getRandomBytesSync(1024 * 1024);
+  const dbPath = `${dataDir}/db`;
+  const db = await DB.createDB({ dbPath, logger });
+  await db.start();
+  const data0 = crypto.randomBytes(0);
+  const data1MiB = crypto.randomBytes(1024 * 1024);
   const summary = await b.suite(
     'DB1MiB',
     b.add('get 1 MiB of data', async () => {
-      await db1.put([], '1mib', data1MiB, true);
+      await db.put([], '1kib', data1MiB, true);
       return async () => {
-        await db1.get([], '1mib', true);
+        await db.get([], '1kib', true);
       };
     }),
     b.add('put 1 MiB of data', async () => {
-      await db1.put([], '1mib', data1MiB, true);
+      await db.put([], '1kib', data1MiB, true);
     }),
-    b.add('get 1 MiB of data with workers', async () => {
-      await db2.put([], '1mib', data1MiB, true);
-      return async () => {
-        await db2.get([], '1mib', true);
-      };
+    b.add('put zero data', async () => {
+      await db.put([], '0', data0, true);
     }),
-    b.add('put 1 MiB of data with workers', async () => {
-      await db2.put([], '1mib', data1MiB, true);
+    b.add('put zero data then del', async () => {
+      await db.put([], '0', data0, true);
+      await db.del([], '0');
     }),
     b.cycle(),
     b.complete(),
@@ -68,13 +50,11 @@ async function main() {
       format: 'chart.html',
     }),
   );
-  await db1.stop();
-  await db2.stop();
+  await db.stop();
   await fs.promises.rm(dataDir, {
     force: true,
     recursive: true,
   });
-  await workerManager.stop();
   return summary;
 }
 
