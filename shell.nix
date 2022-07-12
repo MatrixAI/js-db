fd749a153d3c47777d1fc569d186923b017a2220
Roger Qiu <roger.qiu@polyhack.io>
2022-07-12 19:05:16 +1000

    ci: use `.npmrc` to specify `npm_config_prefix` and `npm_config_jobs`

--- shell.nix
+++ shell.nix
@@ -22,14 +22,8 @@ mkShell {
     # Built executables and NPM executables
     export PATH="$(pwd)/dist/bin:$(npm bin):$PATH"
 
-    # Enables npm link
-    export npm_config_prefix=~/.npm
-
     # Path to headers used by node-gyp for native addons
     export npm_config_nodedir="${nodejs}"
-
-    # Use all cores during node-gyp compilation
-    export npm_config_jobs=max
 
     # Verbose logging of the Nix compiler wrappers
     export NIX_DEBUG=1
