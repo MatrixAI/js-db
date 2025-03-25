3d7370978dc838c278daeb29b9965f645fa3b8cf
Brynley Llewellyn-Roux <blynblack@gmail.com>
2025-03-25 11:03:50 +1100

    fix: revert python fixes and rely on CI fixes

--- scripts/brew-install.sh
+++ scripts/brew-install.sh
@@ -15,9 +15,3 @@ brew link --overwrite node@20
 brew install python@3.10
 brew link --overwrite python@3.10
 
-PYTHON3_PATH=$(brew --prefix python@3.10)/bin/python3
-ln -sf "$PYTHON3_PATH" /opt/homebrew/bin/python3
-
-export PYTHON="$PYTHON3_PATH"
-
-echo "Python version: $(python3 --version)"
